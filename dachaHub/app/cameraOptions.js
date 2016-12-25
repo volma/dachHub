20a2fc514d314a3705acbc68da43c7736df47a5e
Alexei Dmitrenko <alexei.dmitrenko@gmail.com>
2016-12-25 22:34:12 +0300

    'options'

---
+++ dachaHub/app/cameraOptions.js
@@ -0,0 +1,9 @@
+"use strict";
+var CameraOptions = (function () {
+    function CameraOptions() {
+        this.quality = 5;
+    }
+    return CameraOptions;
+}());
+exports.CameraOptions = CameraOptions;
+//# sourceMappingURL=cameraOptions.js.map
